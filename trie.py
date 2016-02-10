3a5432aa9c583a3d3f0f4c6b2fab165eeb4601e6
Pramod Maharjan <unknown.emlap@gmail.com>
2016-02-10 10:42:01 +0545

    Generic Trie for Ngram implemented

--- trie.py
+++ trie.py
@@ -2,23 +2,32 @@ class Node(object):
     def __init__(self):
         self.count = 0
         self.child = {}
+        self.nextRoot = None
 
 
 class Trie:
-    def __init__(self):
+    def __init__(self, n):
+        self.ngram = n
         self.root = Node()
 
     def Insert(self, key):
-        self.root = self.InsertAgain(self.root, key, 0)
+        self.root = self.InsertAgain(self.root, key, 0, 0)
 
-    def InsertAgain(self, node, key, pos):
-        if(pos == len(key)):
-            node.count += 1
-            return node
-        if node.child.get(key[pos]) is None:
-            node.child[key[pos]] = Node()
-        node.child[key[pos]] = self.InsertAgain(node.child[key[pos]],
-            key, pos+1)
+    def InsertAgain(self, node, key, wpos, lpos):
+        if(lpos == len(key[wpos])):
+            if(wpos == self.ngram-1):
+                node.count += 1
+                return node
+            else:
+                if node.nextRoot is None:
+                    node.nextRoot = Node()
+                node.nextRoot = self.InsertAgain(node.nextRoot, key, wpos+1, 0)
+                return node
+        index = key[wpos][lpos]
+        if node.child.get(index) is None:
+            node.child[index] = Node()
+        node.child[index] = self.InsertAgain(node.child[index],
+            key, wpos, lpos+1)
         return node
 
     def Search(self, key):
@@ -27,36 +36,48 @@ class Trie:
         return False
 
     def Count(self, key):
-        return self.SearchAgain(self.root, key, 0)
+        return self.SearchAgain(self.root, key, 0, 0)
 
-    def SearchAgain(self, node, key, pos):
+    def SearchAgain(self, node, key, wpos, lpos):
         if node is None:
             return 0
-        if(pos == len(key)):
-            return node.count
-        if node.child.get(key[pos]) is None:
+        if(lpos == len(key[wpos])):
+            if(wpos == self.ngram-1):
+                return node.count
+            else:
+                if node.nextRoot is None:
+                    return 0
+                return self.SearchAgain(node.nextRoot, key, wpos+1, 0)
+        index = key[wpos][lpos]
+        if node.child.get(index) is None:
             return 0
-        return self.SearchAgain(node.child[key[pos]], key, pos+1)
+        return self.SearchAgain(node.child[index], key, wpos, lpos+1)
 
 def main():
-    data = ['pramod', 'uttam', 'navin', 'prabesh', 'pramod', 'pramodmjn']
-    newTrie = Trie()
-    for x in range(len(data)):
-        newTrie.Insert(list(data[x]))
+    #data = ['pramod', 'uttam', 'navin', 'prabesh', 'pramod', 'pramodmjn']
+    newTrie = Trie(2)
+    newTrie.Insert(['my', 'project'])
+    newTrie.Insert(['my', 'name'])
+    newTrie.Insert(['your', 'project'])
+    newTrie.Insert(['your', 'name'])
+    newTrie.Insert(['my', 'project'])
     print(newTrie.root.child.keys())
-    nodep = newTrie.root.child['p']
-    noder= nodep.child['r']
-    nodea = noder.child['a']
-    nodem = nodea.child['m']
-    nodeo = nodem.child['o']
-    noded = nodeo.child['d']
-    print(noded.count)
-    print(nodea.child.keys())
-    print(newTrie.Search('pramod'))
-    print(newTrie.Search(list('pamod')))
-    print(newTrie.Search(list('saroj')))
-    print(newTrie.Count('pramod'))
-    print("No. of pramodmjn =",newTrie.Count('pramodmjn'))
+
+    print(newTrie.Search(['my', 'name']))
+    print(newTrie.Search(['my', 'project']))
+    print(newTrie.Search(['my', 'Project']))
+    print(newTrie.Count(['my', 'project']))
+
+    oneGram = Trie(1)
+    oneGram.Insert(['pramod'])
+    oneGram.Insert(['navin'])
+    oneGram.Insert(['uttam'])
+    oneGram.Insert(['prabesh'])
+    oneGram.Insert(['pramod'])
+    print(oneGram.Search(['pramod']))
+    print(oneGram.Search(['teskabaje']))
+    print(oneGram.Count(['pramod']))
+    print(oneGram.Count(['prabesh']))
 if __name__=="__main__":
     main()
 
