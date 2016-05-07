c6106a354408d6d1dc4c9090c76bb9e61b47923e
Navin IR <navinayer56@gmail.com>
2016-05-07 14:54:32 +0545

    Updated setup.py

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'aes.apps.AesConfig',
+    # 'aes.apps.AesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
