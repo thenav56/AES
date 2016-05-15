bcec3617021e5f45ca0687a2882ad938030490f7
Navin IR <navinayer56@gmail.com>
2016-05-15 17:39:45 +0545

    Corn Fix use sys.path.append import django

--- .openshift/cron/minutely/corn.py
+++ .openshift/cron/minutely/corn.py
@@ -1,19 +1,21 @@
-import django
-import os
-
-
-on_openshift = False
-if 'OPENSHIFT_APP_NAME' in os.environ:
-    on_openshift = True
-
-if on_openshift:
-    os.chdir(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/myproject'))
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
-django.setup()
 
 
 def train_scheduled_job():
+    import django
+    import os
+    import sys
+
+    on_openshift = False
+    if 'OPENSHIFT_APP_NAME' in os.environ:
+        on_openshift = True
+
+    if on_openshift:
+        sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'],
+                        'wsgi/myproject'))
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+    django.setup()
+
     from essay.models import CronJob
     try:
         cronjob = CronJob.objects.order_by('updated').filter(status='0')[0]
