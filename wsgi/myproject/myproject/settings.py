b3d1fb8e26ea9df121c865192ee828f24bee83d3
Navin IR <navinayer56@gmail.com>
2016-05-15 15:55:36 +0545

    Setting Debug off

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -33,8 +33,8 @@ if on_openshift:
 
 # if on_openshift:
     # DEBUG = False
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
