3fb23e3f7d9625e662b5c0614c53d7019eb186a2
Navin IR <navinayer56@gmail.com>
2016-05-15 16:21:07 +0545

    cron fix

--- .openshift/cron/minutely/corn.py
+++ .openshift/cron/minutely/corn.py
@@ -1,12 +1,19 @@
 import django
 import os
+
+
+on_openshift = False
+if 'OPENSHIFT_APP_NAME' in os.environ:
+    on_openshift = True
+
+if on_openshift:
+    os.chdir(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/myproject'))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 django.setup()
 
 
 def train_scheduled_job():
-    from django.conf import settings
-    os.chdir(settings.BASE_DIR)
     from essay.models import CronJob
     try:
         cronjob = CronJob.objects.order_by('updated').filter(status='0')[0]
